19e65e93fb8b49fb037abce791c5e5818a752c48
iqballbayhaqi <baihaqiiqbal323@gmail.com>
2023-08-07 02:32:52 +0700

    update 0.0.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,22 +18,27 @@ export default function Home() {
             Portfolio
           </button>
           <button>
-            Gallery
-          </button>
-          <button>
             Tentang Kami
           </button>
         </div>
         <div>
-          <button className='navbar-contact-us'>
-            Hubungi Kami!
-          </button>
+          <a href='https://wa.me/6285780200226' target="_blank">
+            <button className='navbar-contact-us'>
+              Hubungi Kami!
+            </button>
+          </a>
         </div>
       </div>
 
       {/* Header */}
       <div className='header-container shapedividers_com-2341'>
+        <div className='header-text-title'>
+          <p className='header-subtext'>Jasa Pasang Kaca Film</p>
+          <p className='header-subtext'>Gedung Berkualitas &</p>
+          <p className='header-subtext'>Terpercaya</p>
 
+          <p className='header-subtext-little'>Jadikan Gedungmu Lebih Eksklusif dengan Kaca Film Berkualitas!</p>
+        </div>
       </div>
 
       {/* Body */}
@@ -42,16 +47,39 @@ export default function Home() {
         <p className='sub-header-1'>Sudah ada 100+ klien puas dengan pekerjaan kami</p>
       </div>
 
-      <div className='row-gallery'>
-        <Image src='/06122013024.jpg' height={400} width={400} alt='jasa pasang kaca film gedung' />
-        <Image src='/06122013029.jpg' height={400} width={400} alt='jasa pasang kaca film gedung' />
-        <Image src='/18112013015.jpg' height={400} width={400} alt='jasa pasang kaca film gedung' />
+      <div style={{marginBottom: 100}}>
+        <div className='row-gallery'>
+          <Image src='/06122013024.jpg' height={400} width={400} alt='jasa pasang kaca film gedung' />
+          <Image src='/06122013029.jpg' height={400} width={400} alt='jasa pasang kaca film gedung' />
+          <Image src='/18112013015.jpg' height={400} width={400} alt='jasa pasang kaca film gedung' />
+        </div>
+
+        <div className='row-gallery'>
+          <Image src='/20112013016.jpg' height={400} width={400} alt='jasa pasang kaca film gedung' />
+          <Image src='/dsfsddsfdsfdsfdsfsfdsfdsf.jpg' height={400} width={400} alt='jasa pasang kaca film gedung' />
+          <Image src='/IMG_20220823_181716.jpg' height={400} width={400} alt='jasa pasang kaca film gedung' />
+        </div>
+
+        <div className='row-gallery'>
+          <Image src='/IMG-20190621-WA0006.jpg' height={400} width={400} alt='jasa pasang kaca film gedung' />
+          <Image src='/IMG-20190623-WA0001.jpg' height={400} width={400} alt='jasa pasang kaca film gedung' />
+          <Image src='/IMG-20190722-WA0009.jpg' height={400} width={400} alt='jasa pasang kaca film gedung' />
+        </div>
+
+        <div className='row-gallery'>
+          <Image src='/IMG-20190722-WA0017.jpg' height={400} width={400} alt='jasa pasang kaca film gedung' />
+          <Image src='/IMG-20190722-WA0018.jpg' height={400} width={400} alt='jasa pasang kaca film gedung' />
+          <Image src='/IMG-20190724-WA0008.jpg' height={400} width={400} alt='jasa pasang kaca film gedung' />
+        </div>
       </div>
 
-      <div className='row-gallery'>
-        <Image src='/20112013016.jpg' height={400} width={400} alt='jasa pasang kaca film gedung' />
-        <Image src='/dsfsddsfdsfdsfdsfsfdsfdsf.jpg' height={400} width={400} alt='jasa pasang kaca film gedung' />
-        <Image src='/IMG_20220823_181716.jpg' height={400} width={400} alt='jasa pasang kaca film gedung' />
+      <div className='shapedividers_com-7830 center shapedividers_com-2341 divider-middle'>
+        <p>Hubungi Kami Sekarang!</p>
+        <a href='https://wa.me/6285780200226' target="_blank">
+          <button style={{marginTop: 20}} className='button-contact-us'>
+            Hubungi Kami
+          </button>
+        </a>
       </div>
       
     </div>
